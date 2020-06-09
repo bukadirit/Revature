63057edf3ab2d2b58fdd5f2d94ab3031a5aab5d3
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-06-08 22:03:19 -0400

    Added a logout method

--- project_1/src/app.ts
+++ project_1/src/app.ts
@@ -1,41 +1,44 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { employeeRouter } from './routers/employee-router';
-import { loginRouter } from './auth/index';
-import { portalRouter } from './routers/portal-router';
-import path from 'path';
-import session from 'express-session';
-const passport =require('passport');
-const flash = require('express-flash')
+import express from "express";
+import bodyParser from "body-parser";
+import { employeeRouter } from "./routers/employee-router";
+import { loginRouter } from "./auth/index";
+import { portalRouter } from "./routers/portal-router";
+import path from "path";
+import session from "express-session";
+const passport = require("passport");
+const flash = require("express-flash");
+const methodOverride = require("method-override");
 
 const app = express();
-const initializePassport = require('./auth/passportConfig');
+const initializePassport = require("./auth/passportConfig");
 initializePassport(passport);
 
-app.use(session({
-    secret: 'dragonite',
+app.use(
+  session({
+    secret: "dragonite",
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash())
-
+app.use(flash());
+app.use(methodOverride("_method"));
 
 const port = process.env.PORT || 3001;
 
-app.set('port', port);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("port", port);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use('/', loginRouter);
-app.use('/portal', portalRouter);
-app.use('/user', employeeRouter);
+app.use("/", loginRouter);
+app.use("/portal", portalRouter);
+app.use("/user", employeeRouter);
 
 app.listen(port, () => {
-    console.log(`Home app running at http://localhost:${port}`);
+  console.log(`Home app running at http://localhost:${port}`);
 });
--- project_1/src/auth/passportConfig.ts
+++ project_1/src/auth/passportConfig.ts
@@ -1,46 +1,40 @@
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require('bcrypt')
-import * as employeeService from '../services/employee-service';
-import path from 'path';
-import express from 'express'
-
+const bcrypt = require("bcrypt");
+import * as employeeService from "../services/employee-service";
+import path from "path";
+import express from "express";
 
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-var user, role,pw;
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+var user, role, pw;
 
-
-async function initialize(passport,){
-    const authenticateUser = async (email,password,done) =>{
-        user =  await employeeService.getEmployee(email);
-        //console.log(user)
-        pw = user.map(e => e.password);
-        role = user.map(e => e.userRole);
-        //console.log(role)
-        if(user.length == 0){
-            return done(null, false, { message: "User Does Not Exist"} )
-        }
-        try{
-            if( await bcrypt.compare(password, pw.toString())){
-                return done(null,user)
-            }else{
-                return done(null, false, { message: 'Incorrect Email or Password' })
-            }
-        }catch(e){
-            return done(e)
-        }
-
+async function initialize(passport) {
+  const authenticateUser = async (email, password, done) => {
+    user = await employeeService.getEmployee(email);
+    //console.log(user)
+    pw = user.map((e) => e.password);
+    role = user.map((e) => e.userRole);
+    //console.log(role)
+    if (user.length == 0) {
+      return done(null, false, { message: "User Does Not Exist" });
     }
+    try {
+      if (await bcrypt.compare(password, pw.toString())) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Incorrect Email or Password" });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
 
-    passport.use(new LocalStrategy({usernameField: 'email'},authenticateUser));
-    passport.serializeUser((role,done) =>done(null,role))
-    passport.deserializeUser((user,done) =>{ 
-        return done(null,user)
-     })
+  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+  passport.serializeUser((role, done) => done(null, role));
+  passport.deserializeUser((user, done) => {
+    return done(null, user);
+  });
 }
 
-
-
-
-module.exports =initialize;
+module.exports = initialize;
